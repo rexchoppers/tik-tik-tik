376a9982b3cb0835b494f0a1383b92fc6b655b21
rexchoppers <connorbrady@codox.uk>
2025-05-20 15:39:58 +0100

    ADDED Beep for leap seconds

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ import ntplib
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 from TTS.api import TTS
+from pydub import AudioSegment
+from pydub.generators import Sine
 
 # Every 10 seconds, the voice announces the time in the UK
 SPEAKING_INTERVAL = 10
+BEEPS = 3
 
 # Get the current time in the UK
 def get_uk_time():
@@ -28,9 +31,33 @@ def retrieve_ntp_time():
     response = ntp_client.request('uk.pool.ntp.org', version=3)
     return response
 
+def make_beep_sequence(filename: str, leap: bool = False):
+    beep = Sine(1000).to_audio_segment(duration=250).apply_gain(-3)
+    silence = AudioSegment.silent(duration=750)
+
+    sequence = AudioSegment.silent(duration=0)
+    for i in range(BEEPS):
+        sequence += beep
+
+        beep_count = i + 1
+
+        if i < BEEPS - 1:
+            # Check if leap and if it's between beep 2 and 3
+            if leap and (beep_count == 2 or beep_count == 3):
+                # Add a longer silence
+                sequence += AudioSegment.silent(duration=1000)
+            else:
+                # Add a normal silence
+                sequence += silence
+
+    sequence.export(filename, format="wav")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    # Generate beeps on application start
+    make_beep_sequence("beep.wav")
+    make_beep_sequence("beep_leap.wav", leap=True)
+
     while True:
         now = datetime.now()
 
@@ -40,6 +67,3 @@ if __name__ == '__main__':
         time.sleep(sleep_secs)
 
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-# tts --model_name "tts_models/uk/mai/vits" --download
